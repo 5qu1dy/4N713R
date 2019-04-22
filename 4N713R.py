f780076d848b1d1249bfd672828f5c51bfceabc1
5qu1dy <48722600+5qu1dy@users.noreply.github.com>
2019-04-22 18:03:42 +0700

    Add files via upload

---
+++ 4N713R.py
@@ -0,0 +1,197 @@
+#MAU APA LU?
+#MAU LIAT PASSWORD?
+#APA MAU RECODE?
+#JAN DI RECODE YA NJING!
+#AWAS LU KALO KETAUAN NGE RECODE TOOLS GW
+
+import os
+import time
+
+os.system ("clear")
+os.system ("toilet -f mono12 -F gay '4N713R'")
+print ("\033[1;33m[•]••••••••••••••••••••••••••••••••••••••••••[•]")
+print ("\033[1;33m[•]            Author : Mr.Squidy            [•]")
+print ("\033[1;33m[•]        Team : Garuda Team Comando        [•]")
+print ("\033[1;34m[•]    Github : https://github.com/5qu1dy    [•]")
+print ("\033[1;34m[•]         Contact : +6281393175636         [•]")
+print ("\033[1;34m[•]••••••••••••••••••••••••••••••••••••••••••[•]")
+
+print ("\033[1;32m   Passwordnya? :v")
+pw=input ("   ")
+if pw=="ikan tongkol":
+   time.sleep(1)
+   print ("   Password Correct ✅")
+   time.sleep(1)
+   print ("   Okay Tunggu!!")
+   os.system ("clear")
+   print ("\033[1;36m   10%  •")
+   time.sleep(1)
+   os.system ("clear")
+   print ("\033[1;36m   20%  ••")
+   time.sleep(0.5)
+   os.system ("clear")
+   print ("\033[1;36m   30%  •••")
+   time.sleep(2)
+   os.system ("clear")
+   print ("\033[1;36m   40%  ••••")
+   time.sleep(0.2)
+   os.system ("clear")
+   print ("\033[1;36m   50%  •••••")
+   time.sleep(3)
+   os.system ("clear")
+   print ("\033[1;36m   60%  ••••••")
+   time.sleep(0.7)
+   os.system ("clear")
+   print ("\033[1;36m   70%  •••••••")
+   time.sleep(1)
+   os.system ("clear")
+   print ("\033[1;36m   80%  ••••••••")
+   time.sleep(3)
+   os.system ("clear")
+   print ("\033[1;36m   90%  •••••••••")
+   time.sleep(0.4)
+   os.system ("clear")
+   print ("\033[1;36m   100% ••••••••••")
+   time.sleep(3)
+   print ("\033[1;36m   Loading dah selesai ea")
+   time.sleep(2)
+   os.system ("clear")
+   time.sleep(1)
+   os.system ("toilet -f mono12 -F gay '4N713R'")
+   print ("\033[1;33m[•]••••••••••••••••••••••••••••••••••••••••••[•]")
+   print ("\033[1;33m[•]            Author : Mr.Squidy            [•]")
+   print ("\033[1;33m[•]        Team : Garuda Team Comando        [•]")
+   print ("\033[1;34m[•]    Github : https://github.com/5qu1dy    [•]")
+   print ("\033[1;34m[•]         Contact : +6281393175636         [•]")
+   print ("\033[1;34m[•]••••••••••••••••••••••••••••••••••••••••••[•]")
+   menu='''
+      1. Install OSIF
+      2. Install Metasploit
+      3. Install DDos Tools
+      4. Install Tool-X
+      5. Install Ubuntu
+      6. Install SQL Map
+      7. Install Matrix
+      8. Install RedHawk
+      9. Install KaliNethunter
+      10.Install SocialFish
+
+      0. Exit
+   '''
+   print ("\033[1;32m   Menu Pilihan yang ANJIER :v")
+   print (menu)
+   print ("\033[1;31m      Masukkan pilihan ter ANJIER anda!!")
+   slc=input ("      >> ")
+   if slc=="1" or slc=="01":
+      print (" ")
+      print ("      SABAR YA NYED")
+      time.sleep(1)
+      print ("      INI LAGI DIINSTALL KOK")
+      time.sleep(2)
+      os.system ("apt update;apt upgrade;apt install python2;pip2 install requests;apt install git;git clone https://github.com/CiKu370/OSIF.git;cd OSIF;pip2 install -r requirements.txt;python2 osif.py")
+
+   elif slc=="3" or slc=="03":
+        print (" ")
+        print ("      SABAR YA NYED")
+        time.sleep(1)
+        print ("      INI LAGI DIINSTALL KOK")
+        time.sleep(2)
+        os.system ("apt upgrade;apt update;apt install git;apt install python2;https://github.com/Cr4bbyP4tty/DDos-Attack;cd DDos-Attack;python2 ddos-attack.py")
+
+   elif slc=="4" or slc=="04":
+        print (" ")
+        print ("      SABAR YA NYED")
+        time.sleep(1)
+        print ("      INI LAGI DIINSTALL KOK")
+        time.sleep(2)
+        os.system ("apt update;apt upgrade;apt install git;git clone https://github.com/Rajkumrdusad/Tool-X;cd Tool-X;chmod +x *;sh install.aex")
+   elif slc=="5" or slc=="05":
+        print (" ")
+        print ("      SABAR YA NYED")
+        time.sleep(1)
+        print ("      INI LAGI DIINSTALL KOK")
+        time.sleep(2)
+        os.system ("apt update;apt upgrade;apt install git;apt install proot;apt install wget;git clone https://github.com/Neo-Oli/termux-ubuntu;cd termux-ubuntu;chmod+x *;sh ubuntu.sh")
+   elif slc=="6" or slc=="06":
+        print (" ")
+        print ("      SABAR YA NYED")
+        time.sleep(1)
+        print ("      INI LAGI DIINSTALL KOK")
+        time.sleep(2)
+        os.system ("apt update;apt upgrade;apt install git;apt install python2;apt install python;git clone https://github.com/sqlmapproject/sqlmap;cd sqlmap;chmod+x *;python2 sqlmap.py -h")
+   elif slc=="7" or slc=="07":
+        print (" ")
+        print ("      SABAR YA NYED")
+        time.sleep(1)
+        print ("      INI LAGI DIINSTALL KOK")
+        time.sleep(2)
+        os.system ("apt update;apt upgrade;apt install cmatrix;cmatrix")
+   elif slc=="8" or slc=="08":
+        print (" ")
+        print ("      SABAR YA NYED")
+        time.sleep(1)
+        print ("      INI LAGI DIINSTALL KOK")
+        time.sleep(2)
+        os.system ("apt install git;apt install php;git clone https://github.com/Tuhinshubhra/RED_HAWK;cd RED_HAWK;chmod +x *;php rhawk.php")
+   elif slc=="9" or slc=="09":
+        print (" ")
+        print ("      SABAR YA NYED")
+        time.sleep(1)
+        print ("      INI LAGI DIINSTALL KOK")
+        time.sleep(2)
+        os.system ("apt update;apt upgrade;git clone https://github.com/Hax4us/Nethunter-In-Termux;cd Nethunter-In-Termux;chmod +x *;./kalinethunter")
+   elif slc=="10":
+        print (" ")
+        print ("      SABAR YA NYED")
+        time.sleep(1)
+        print ("      INI LAGI DIINSTALL KOK")
+        time.sleep(2)
+        os.system ("apt update;apt upgrade;apt install git;apt install python2;git clone https://github.com/UndeadSec/SocialFish;cd SocualFish;chmod +x *;pip2 install -r requirements.txt;python2 SocialFish.py")
+   elif slc=="0" or slc=="00":
+        print ("SABAR CUK...")
+        time.sleep(1)
+        print ("Baru CLOSET!")
+        time.sleep(1)
+        print ("Eh maap TYPO.. maksud w 'Baru Closing'")
+        time.sleep(2)
+        os.system ("exit")
+
+   else:
+        print ("\033[0;31m      ERROR : Wrong Input...!")
+        os.system ("exit")
+
+else:
+    time.sleep(2)
+    print ("\033[1;31m   PASSWORD SALAH!!!")
+    ha=input ("\033[1;36m   Mau tau passwordnya? [Y/n] ")
+    if ha=="Y" or ha=="y":
+       time.sleep(1)
+       print ("\033[1;32m   ASHIYAAAAAAPPPP!!!")
+       hi=input ("   Pengen Tau Caranya?? [Y/n] ")
+       if hi=="Y" or hi=="y":
+          time.sleep(1)
+          print ("\033[1;32m1. Copy Link di bawah ini !1!1!1")
+          time.sleep(2)
+          print ("\033[1;33mhttps://api.whatsapp.com/send?phone=6281393175636&text=Coy!+Minta+Password+Tools+ANJIR+dong+;V")
+          time.sleep(2)
+          print ("\033[1;32m2. Paste di gugel atau brownies kalian")
+          time.sleep(1)
+          print ("\033[1;35meh maap typo.. maksud w bukan brownies tapi browser.. hehe")
+          time.sleep(2)
+          print ("\033[1;32m3. Lalu buka!")
+          time.sleep(1)
+          print ("\033[1;32mOKE, SEKIAN DAN TERIMA KASIH!")
+       elif hi=="N" or hi=="n":
+            time.sleep(2)
+            print ("\033[1;31m   Yasudahlah ;(")
+            time.sleep(10)
+       else:
+           time.sleep(1)
+           print ("\033[1;35m   Masukin Input yang bener Nyed ")
+    elif ha=="N" or ha=="n":
+         time.sleep(2)
+         print ("\033[1;31m   Yasudahlah ;(")
+         time.sleep(10)
+    else:
+        time.sleep(1)
+        print ("\033[1;35m   Masukin Input yang bener Nyed ")
